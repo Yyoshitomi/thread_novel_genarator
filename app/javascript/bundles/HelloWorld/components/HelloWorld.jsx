6cc59c7a3d1d0b2b41fa00006e9f07eda42a2288
Yyoshitomi <42134813+Yyoshitomi@users.noreply.github.com>
2020-09-13 15:13:03 +0900

    jsxファイルにレスを直書き

--- app/javascript/bundles/HelloWorld/components/HelloWorld.jsx
+++ app/javascript/bundles/HelloWorld/components/HelloWorld.jsx
@@ -6,14 +6,29 @@ const HelloWorld = (props) => {
 
   return (
     <div>
-      <h3>Hello, {name}!</h3>
       <hr />
-      <form>
+      <b>
+        9 名前：本当にあった怖い名無し 投稿日：2008/05/19(月) 15:49:43 ID:NANtEKit0
+      </b>
+      <p>
+        だが次の瞬間、お守りが眩い光に包まれ、どこからとも無くTさんの声が
+        「破ぁ！！」
+        お守りは光と共に飛び散り、男の半身を吹き飛ばした。
+        「あああああああああ」
+        半身でのたうつ男を尻目に俺は夢から目覚めた
+        枕元にあったはずのお守りはどこをどんなに探しても見つからなかった・・・
+        その話をTさんに話すと
+        「半身を吹き飛ばした？
+        やれやれ、威力は親父の作った奴の半分か・・・」と呟くTさん
+        寺生まれはスゴイ、俺は感動を覚えずにはいられなかった。
+      </ p>
+      <hr />
+      {/* <form>
         <label htmlFor="name">
           Say hello to:
           <input id="name" type="text" value={name} onChange={(e) => setName(e.target.value)} />
         </label>
-      </form>
+      </form> */}
     </div>
   );
 };
