c1f61c60aafd2980252ee8a0b2ee66bd1f3100af
Yyoshitomi <42134813+Yyoshitomi@users.noreply.github.com>
2020-09-19 18:20:22 +0900

    propsでレスを受け取れるようにした

--- app/controllers/hello_world_controller.rb
+++ app/controllers/hello_world_controller.rb
@@ -3,8 +3,30 @@
 class HelloWorldController < ApplicationController
 
   def index
-    @hello_world_props = { name: "Stranger" }
+    honbun =<<-EOS
+          だが次の瞬間、お守りが眩い光に包まれ、どこからとも無くTさんの声が
+          「破ぁ！！」
+          お守りは光と共に飛び散り、男の半身を吹き飛ばした。
+          「あああああああああ」
+          半身でのたうつ男を尻目に俺は夢から目覚めた
+          枕元にあったはずのお守りはどこをどんなに探しても見つからなかった・・・
+          その話をTさんに話すと
+          「半身を吹き飛ばした？
+          やれやれ、威力は親父の作った奴の半分か・・・」と呟くTさん
+          寺生まれはスゴイ、俺は感動を覚えずにはいられなかった。
+EOS
+
+    @hello_world_props = 
+    { 
+      name: "本当にあった怖い名無し",
+      number: 108,
+      date: "2008/05/19(月) 15:49:43",
+      ch_id: "NANtEKit0",
+      honbun: honbun
+    }
   end
+
+  
 
   def test
   end
--- app/javascript/bundles/Responce/components/Responce.jsx
+++ app/javascript/bundles/Responce/components/Responce.jsx
@@ -1,42 +1,31 @@
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
+import { Card } from 'react-bootstrap';
 
-const Responce = (props) => {
-  const [name, setName] = useState(props.name);
+
+const Responce = ({number, name, date, ch_id, honbun}) => {
+  
 
   return (
-    <dl>
-      <dt>
-        9 名前：本当にあった怖い名無し 投稿日：2008/05/19(月) 15:49:43 ID:NANtEKit0
-      </dt>
-        <dd>
-          だが次の瞬間、お守りが眩い光に包まれ、どこからとも無くTさんの声が
-            「破ぁ！！」
-          お守りは光と共に飛び散り、男の半身を吹き飛ばした。
-          「あああああああああ」
-          半身でのたうつ男を尻目に俺は夢から目覚めた
-          枕元にあったはずのお守りはどこをどんなに探しても見つからなかった・・・
-          その話をTさんに話すと
-          「半身を吹き飛ばした？
-          やれやれ、威力は親父の作った奴の半分か・・・」と呟くTさん
-          寺生まれはスゴイ、俺は感動を覚えずにはいられなかった。
-        </dd>
-      <dt>
-        9 名前：本当にあった怖い名無し 投稿日：2008/05/19(月) 15:49:43 ID:NANtEKit0
-      </dt>
-        <dd>
-          春休みに入ったばかりのこと、いい天気に誘われてじいちゃんの家にバイクで行った。
-          まだ寒かったけど、広縁はぽかぽかと気持ちよく、そこでしばらく寛いでいた。そうしたら、
-          「ぽぽ、ぽぽっぽ、「破ぁーーーーーー！！」
-          掛け声と共にＴさんの両手から青白い閃光が飛び出し、何かを粉々に打ち砕きました。
-          寺生まれってスゴイ、改めてそう思いました。
-        </dd>
-    </dl>
+    <Card>
+      <Card.Body>
+        <Card.Title>
+          {number} 名前：{name} 投稿日：{date} ID:{ch_id}
+        </Card.Title>
+        <Card.Text>
+          {honbun}
+        </Card.Text>
+      </Card.Body>
+    </Card>
   );
 };
 
 Responce.propTypes = {
-  name: PropTypes.string.isRequired, // this is passed from the Rails view
+  number: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  ch_id: PropTypes.string.isRequired,
+  honbun: PropTypes.string.isRequired
 };
 
 export default Responce;
