b5b3dd2757aa5f24ff0d025c6c1d003afe3c47d4
Yyoshitomi <42134813+Yyoshitomi@users.noreply.github.com>
2020-10-17 19:07:13 +0900

    不要なrootを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,13 @@
 Rails.application.routes.draw do
-  namespace :api do
-    get 'posts/create'
-  end
-  get 'hello_world', to: 'hello_world#index'
-  get 'test', to: 'hello_world#test'
+  
+  resources :topics
 
   namespace :api do
     resources :topics do
       get :generate_id, on: :collection
     end
+    resources :posts
   end
 
-  resources :topics
   # For details on the DSL availablea within this file, see https://guides.rubyonrails.org/routing.html
 end
