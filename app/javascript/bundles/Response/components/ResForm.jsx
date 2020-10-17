c017e9ccf8e66def6427449a04ccb6e3ce9b28a9
Yyoshitomi <42134813+Yyoshitomi@users.noreply.github.com>
2020-10-17 15:34:25 +0900

    入力フォームをCard内に設置

--- app/javascript/bundles/Response/components/ResForm.jsx
+++ app/javascript/bundles/Response/components/ResForm.jsx
@@ -7,6 +7,7 @@ import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
 import Dropdown from 'react-bootstrap/Dropdown'
 import moment from "moment";
+import Card from 'react-bootstrap/Card'
 
 import { fetchEntity } from "../../../libs/requestManager";
 
@@ -43,98 +44,100 @@ const ResForm = ({ threadConfig }) => {
   })
 
     return (
-      <Form onSubmit={formik.handleSubmit}>
-        <Form.Row>
-          <Form.Group as={Col} md="2">
-            <Form.Label>レス番</Form.Label>
-            <FormControl
-              type="text"
-              name="number"
-              value={formik.values.number}
-              onChange={formik.handleChange}
-            />
-          </Form.Group>
+      <Card>
+        <Card body onSubmit={formik.handleSubmit}>
+          <Form.Row>
+            <Form.Group as={Col} md="2">
+              <Form.Label>レス番</Form.Label>
+              <FormControl
+                type="text"
+                name="number"
+                value={formik.values.number}
+                onChange={formik.handleChange}
+              />
+            </Form.Group>
 
-          <Form.Group as={Col} md="6">
-            <Form.Label>名前</Form.Label>
-            <FormControl
-              type="text"
-              name="name"
-              value={formik.values.name}
-              onChange={formik.handleChange}
-            />
-          </Form.Group>
-          <Form.Group as={Col} md="4">
-            <Dropdown>
-              <Dropdown.Toggle className="mt-4" variant="success" id="dropdown-basic">
-                固定ハンドルネーム
-                </Dropdown.Toggle>
-              <Dropdown.Menu>
-                <Dropdown.Item href="#">既存のコテハン</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          </Form.Group>
-        </Form.Row>
-        <Form.Row>
-          {formik.initialValues.timeDisplay && (
-            <>
-              <Form.Group as={Col} md="4">
-                <Form.Label>投稿日</Form.Label>
-                <FormControl
-                  type="text"
-                  name="date"
-                  value={formik.values.date}
-                  onChange={formik.handleChange}
-                />
-              </Form.Group>
-              <Form.Group as={Col} md="2">
-                <Form.Check
-                  className="mt-5"
-                  type="checkbox"
-                  name="date_mojibake"
-                  checked={formik.values.check}
-                  // onPress={() => setFieldValue('check', !values.check)}
-                  label="文字化け"
-                />
-              </Form.Group>
-            </>)}
+            <Form.Group as={Col} md="6">
+              <Form.Label>名前</Form.Label>
+              <FormControl
+                type="text"
+                name="name"
+                value={formik.values.name}
+                onChange={formik.handleChange}
+              />
+            </Form.Group>
+            <Form.Group as={Col} md="4">
+              <Dropdown>
+                <Dropdown.Toggle className="mt-4" variant="success" id="dropdown-basic">
+                  固定ハンドルネーム
+                  </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <Dropdown.Item href="#">既存のコテハン</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Form.Group>
+          </Form.Row>
+          <Form.Row>
+            {formik.initialValues.timeDisplay && (
+              <>
+                <Form.Group as={Col} md="4">
+                  <Form.Label>投稿日</Form.Label>
+                  <FormControl
+                    type="text"
+                    name="date"
+                    value={formik.values.date}
+                    onChange={formik.handleChange}
+                  />
+                </Form.Group>
+                <Form.Group as={Col} md="2">
+                  <Form.Check
+                    className="mt-5"
+                    type="checkbox"
+                    name="date_mojibake"
+                    checked={formik.values.check}
+                    // onPress={() => setFieldValue('check', !values.check)}
+                    label="文字化け"
+                  />
+                </Form.Group>
+              </>)}
 
-          <Form.Group as={Col} md="4">
-            <Form.Label>ID</Form.Label>
-            <FormControl
-              type="text"
-              name="ch_id"
-              value={formik.values.ch_id}
-              onChange={formik.handleChange}
-            />
-          </Form.Group>
-          <Form.Group as={Col} md="2">
-            <Form.Check
-              className="mt-5"
-              type="checkbox"
-              name="chId_mojibake"
-              checked={formik.values.check}
-              // onPress={() => setFieldValue('check', !values.check)}
-              label="文字化け"
-            />
-          </Form.Group>
-        </Form.Row>
+            <Form.Group as={Col} md="4">
+              <Form.Label>ID</Form.Label>
+              <FormControl
+                type="text"
+                name="ch_id"
+                value={formik.values.ch_id}
+                onChange={formik.handleChange}
+              />
+            </Form.Group>
+            <Form.Group as={Col} md="2">
+              <Form.Check
+                className="mt-5"
+                type="checkbox"
+                name="chId_mojibake"
+                checked={formik.values.check}
+                // onPress={() => setFieldValue('check', !values.check)}
+                label="文字化け"
+              />
+            </Form.Group>
+          </Form.Row>
 
-        <Form.Row>
-          <Form.Group as={Col}>
-            <Form.Control
-              as="textarea"
-              name="honbun"
-              value={formik.values.honbun}
-              onChange={formik.handleChange}
-              rows="3"
-              placeholder="本文を入力してください"
-            />
-          </Form.Group>
-        </Form.Row>
+          <Form.Row>
+            <Form.Group as={Col}>
+              <Form.Control
+                as="textarea"
+                name="honbun"
+                value={formik.values.honbun}
+                onChange={formik.handleChange}
+                rows="3"
+                placeholder="本文を入力してください"
+              />
+            </Form.Group>
+          </Form.Row>
 
-        <Button className="float-right" type="submit">投稿する</Button>
-      </Form>
+          <Button className="float-right" type="submit">投稿する</Button>
+        </Card>
+      </Card>
   );
 };
 
