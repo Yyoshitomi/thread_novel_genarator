6119eee313604ebd141446446b02f3fa3472293e
Yyoshitomi <42134813+Yyoshitomi@users.noreply.github.com>
2020-10-19 12:20:24 +0900

    削除機能追加

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -4,10 +4,36 @@ class TopicsController < ApplicationController
     @topics = Topic.all
   end
 
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new(topic_params)
+    @topic.save
+      redirect_to topics_path
+  end
+
+  def edit
+  end
+
   def show
     @topic = Topic.find(params[:id])
     @props = render_to_string(template: "api/topic/show.json.jbuilder", format: :json)
     @threadConfig = render_to_string(template: "api/topic/config.json.jbuilder", format: :json)
   end
+
+  def destroy
+    @topic = Topic.find(params[:id])
+    @topic.destroy
+      redirect_to topics_path, :notice => "Your post has been deleted successfully."
+  end
+
+  private
+
+  def topic_params
+    params.require(:topic).permit(:title, :default_name, :time_display)
+  end
+
 
 end
